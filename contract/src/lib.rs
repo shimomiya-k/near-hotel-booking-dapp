4c93e0ea2e70d10b2a559144fa01548174fc4361
Kazuma Shimomiya <shimomiya_@indent.co.jp>
2022-09-07 23:10:29 +0900

    section-1-Lesson-1

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -7,66 +7,53 @@
  */
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{log, near_bindgen};
+use near_sdk::collections::LookupMap;
+use near_sdk::json_types::U128;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{env, near_bindgen, AccountId, Promise};
 
-// Define the default message
-const DEFAULT_MESSAGE: &str = "Hello";
+use std::collections::HashMap;
 
-// Define the contract structure
+type RoomId = String;
+type CheckInDate = String;
+
+#[derive(Serialize, Deserialize, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
+#[serde(crate = "near_sdk::serde")]
+pub enum UsageStatus {
+    // 空室
+    Available,
+    // 滞在中
+    Stay { check_in_date: CheckInDate },
+}
+
+#[derive(Serialize, Deserialize, Debug, BorshSerialize, BorshDeserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct RegisteredRoom {
+    name: String,
+    image: String,
+    beds: u8,
+    description: String,
+    location: String,
+    price: U128,
+    status: UsageStatus,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct Room {
+    name: String,
+    owner_id: AccountId,
+    image: String,
+    beds: u8,
+    description: String,
+    location: String,
+    price: U128,
+    status: UsageStatus,
+    booked_info: HashMap<CheckInDate, AccountId>,
+}
+
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshSerialize, BorshDeserialize)]
 pub struct Contract {
-    message: String,
-}
-
-// Define the default, which automatically initializes the contract
-impl Default for Contract{
-    fn default() -> Self{
-        Self{message: DEFAULT_MESSAGE.to_string()}
-    }
-}
-
-// Implement the contract structure
-#[near_bindgen]
-impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
-    pub fn get_greeting(&self) -> String {
-        return self.message.clone();
-    }
-
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, message: String) {
-        // Use env::log to record logs permanently to the blockchain!
-        log!("Saving greeting {}", message);
-        self.message = message;
-    }
-}
-
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(
-            contract.get_greeting(),
-            "Hello".to_string()
-        );
-    }
-
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(
-            contract.get_greeting(),
-            "howdy".to_string()
-        );
-    }
+    rooms_per_owner: LookupMap<AccountId, Vec<RoomId>>,
+    rooms_by_id: HashMap<RoomId, Room>,
 }
